2530cbe91a159c13f99542605b06f265aa37db30
snowleopardAlexa <alexaslomski@gmail.com>
2023-03-24 15:23:16 -0400

    add navigation links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
 //import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Homepage = () => {
   return (
     <>
       <Head>
@@ -15,8 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-      
+         <h1 className="text-3xl font-bold">Home page</h1>
       </main>
     </>
   )
 }
+
+export default Homepage;
