eb49e134d8262aa15e455a70ff1e7ddc3f1a06a6
snowleopardAlexa <alexaslomski@gmail.com>
2023-03-24 17:35:07 -0400

    create PostCard component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c2d9198e1cb520b"; }
+/******/ 		__webpack_require__.h = function() { return "964fea9a391669e8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
