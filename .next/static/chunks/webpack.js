f0e563d160683b9bb16a3d885bc83f66f321a07b
snowleopardAlexa <alexaslomski@gmail.com>
2023-03-24 23:05:53 -0400

    fix full width container with social medias

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fde8fbe694236ae8"; }
+/******/ 		__webpack_require__.h = function() { return "828fe52a623575f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
